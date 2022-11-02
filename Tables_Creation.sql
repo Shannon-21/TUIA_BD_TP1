02d1acf6bfe5e9b6854cd6d18fd699838727ad37
Shannon <81629492+Shannon-21@users.noreply.github.com>
2022-11-02 16:11:58 -0300

    Paso a mayusculas los tipo de datos

--- Tables_Creation.sql
+++ Tables_Creation.sql
@@ -15,61 +15,62 @@
 -- Create the tables
 
 	CREATE TABLE Sucursal (
-		ID_Sucursal int PRIMARY KEY NOT NULL,
-		Ubicacion varchar(20),
+		ID_Sucursal INT PRIMARY KEY NOT NULL,
+		Ubicacion VARCHAR(20),
 	);
 
 
 	CREATE TABLE Sala (
-		ID_Sala int PRIMARY KEY NOT NULL,
-		ID_Sucursal int FOREIGN KEY REFERENCES Sucursal(ID_Sucursal),
+		ID_Sala INT PRIMARY KEY NOT NULL,
+		ID_Sucursal INT FOREIGN KEY REFERENCES Sucursal(ID_Sucursal),
 	);
 
 
 	CREATE TABLE Genero (
-		ID_Genero int PRIMARY KEY NOT NULL,
-		Nombre varchar(20),
+		ID_Genero INT PRIMARY KEY NOT NULL,
+		Nombre VARCHAR(20),
 	);
 
 
 	CREATE TABLE Pelicula (
-		ID_Pelicula int PRIMARY KEY NOT NULL,
-		ID_Genero int FOREIGN KEY REFERENCES Genero(ID_Genero),
-		Nombre varchar(50),
+		ID_Pelicula INT PRIMARY KEY NOT NULL,
+		ID_Genero INT FOREIGN KEY REFERENCES Genero(ID_Genero),
+		Nombre VARCHAR(50),
 		Subtitulos BIT,
 		APT BIT,
 	);
 
 
 	CREATE TABLE Funciones (
-		ID_Funcion int PRIMARY KEY NOT NULL,
-		ID_Pelicula int FOREIGN KEY REFERENCES Pelicula(ID_Pelicula),
-		ID_Sala int FOREIGN KEY REFERENCES Sala(ID_Sala),
-		Fecha date,
-		Horario time,
+		ID_Funcion INT PRIMARY KEY NOT NULL,
+		ID_Pelicula INT FOREIGN KEY REFERENCES Pelicula(ID_Pelicula),
+		ID_Sala INT FOREIGN KEY REFERENCES Sala(ID_Sala),
+		Fecha DATE,
+		Horario TIME,
 	);
 
 
 	CREATE TABLE Usuario (
-		DNI int PRIMARY KEY NOT NULL,
-		Nombre varchar(50),
-		Correo varchar(50),
+		DNI INT PRIMARY KEY NOT NULL,
+		Nombre VARCHAR(50),
+		Correo VARCHAR(50),
 	);
 
 
 	CREATE TABLE Butaca (
-		ID_Butaca varchar(10) PRIMARY KEY NOT NULL,
-		ID_Sala int FOREIGN KEY REFERENCES Sala(ID_Sala),
-		Fila int,
-		Columna varchar(1),
+		ID_Butaca VARCHAR(10) PRIMARY KEY NOT NULL,
+		ID_Sala INT FOREIGN KEY REFERENCES Sala(ID_Sala),
+		Fila INT,
+		Columna VARCHAR(1),
 	);
 
 
 	CREATE TABLE Entrada (
-		ID_Entrada int PRIMARY KEY NOT NULL,
-		ID_Butaca varchar(10) FOREIGN KEY REFERENCES Butaca(ID_Butaca),
-		ID_Funcion int FOREIGN KEY REFERENCES Funciones(ID_Funcion),
-		DNI int FOREIGN KEY REFERENCES Usuario(DNI),
-		Precio money,
+		ID_Entrada INT PRIMARY KEY NOT NULL,
+		ID_Butaca VARCHAR(10) FOREIGN KEY REFERENCES Butaca(ID_Butaca),
+		ID_Funcion INT FOREIGN KEY REFERENCES Funciones(ID_Funcion),
+		DNI INT FOREIGN KEY REFERENCES Usuario(DNI),
+		Precio MONEY,
 	);
+
 
