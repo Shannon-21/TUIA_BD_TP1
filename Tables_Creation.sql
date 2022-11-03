09fb909e245e0b1f1f6e98a181a10dbdaaacf40f
Ezequiel Arevalo <ezequiel.arevalo@rappi.com>
2022-11-03 19:44:31 -0300

    adding compose keys

--- Tables_Creation.sql
+++ Tables_Creation.sql
@@ -2,75 +2,78 @@
 
   -- Remove the database if exists another with the same name
   
-	USE master;
-	DROP DATABASE IF EXISTS BD1_TP1_TUIA;
+USE master;
+DROP DATABASE IF EXISTS BD1_TP1_TUIA;
 
   -- Create the database and select it to work 
 
-	CREATE DATABASE BD1_TP1_TUIA;
-	USE BD1_TP1_TUIA;
-
-
+CREATE DATABASE BD1_TP1_TUIA;
+USE BD1_TP1_TUIA;
 
 -- Create the tables
 
-	CREATE TABLE Sucursal (
-		ID_Sucursal INT PRIMARY KEY NOT NULL,
-		Ubicacion VARCHAR(20),
-	);
+CREATE TABLE Sucursal (
+	ID_Sucursal INT PRIMARY KEY NOT NULL,
+	Ubicacion VARCHAR(20),
+);
 
 
-	CREATE TABLE Sala (
-		ID_Sala INT PRIMARY KEY NOT NULL,
-		ID_Sucursal INT FOREIGN KEY REFERENCES Sucursal(ID_Sucursal),
-	);
+CREATE TABLE Sala (
+	ID_Sala INT NOT NULL,
+	ID_Sucursal INT FOREIGN KEY REFERENCES Sucursal(ID_Sucursal),
+	CONSTRAINT pk_sala PRIMARY KEY (ID_Sala, ID_Sucursal)
+);
 
 
-	CREATE TABLE Genero (
-		ID_Genero INT PRIMARY KEY NOT NULL,
-		Nombre VARCHAR(20),
-	);
+CREATE TABLE Genero (
+	ID_Genero INT PRIMARY KEY NOT NULL,
+	Nombre VARCHAR(20),
+);
 
 
-	CREATE TABLE Pelicula (
-		ID_Pelicula INT PRIMARY KEY NOT NULL,
-		ID_Genero INT FOREIGN KEY REFERENCES Genero(ID_Genero),
-		Nombre VARCHAR(50),
-		Subtitulos BIT,
-		APT BIT,
-	);
+CREATE TABLE Pelicula (
+	ID_Pelicula INT PRIMARY KEY NOT NULL,
+	ID_Genero INT FOREIGN KEY REFERENCES Genero(ID_Genero),
+	Nombre VARCHAR(50),
+	Subtitulos BIT,
+	APT BIT,
+);
 
 
-	CREATE TABLE Funciones (
-		ID_Funcion INT PRIMARY KEY NOT NULL,
-		ID_Pelicula INT FOREIGN KEY REFERENCES Pelicula(ID_Pelicula),
-		ID_Sala INT FOREIGN KEY REFERENCES Sala(ID_Sala),
-		Fecha DATE,
-		Horario TIME,
-	);
+CREATE TABLE Funciones (
+	ID_Funcion INT PRIMARY KEY NOT NULL,
+	ID_Pelicula INT FOREIGN KEY REFERENCES Pelicula(ID_Pelicula),
+	ID_Sala INT ,
+	ID_Sucursal INT,
+	Fecha DATE,
+	Horario TIME,
+	CONSTRAINT FK_SALA FOREIGN KEY (Id_Sala,ID_Sucursal) REFERENCES SALA(Id_Sala,Id_sucursal)
+);
 
 
-	CREATE TABLE Usuario (
-		DNI INT PRIMARY KEY NOT NULL,
-		Nombre VARCHAR(50),
-		Correo VARCHAR(50),
-	);
+CREATE TABLE Usuario (
+	DNI INT PRIMARY KEY NOT NULL,
+	Nombre VARCHAR(50),
+	Correo VARCHAR(50),
+);
 
 
-	CREATE TABLE Butaca (
-		ID_Butaca VARCHAR(10) PRIMARY KEY NOT NULL,
-		ID_Sala INT FOREIGN KEY REFERENCES Sala(ID_Sala),
-		Fila INT,
-		Columna VARCHAR(1),
-	);
+CREATE TABLE Butaca (
+	ID_Butaca VARCHAR(10) NOT NULL,
+	ID_Sala INT,
+	Id_Sucursal INT,
+	Fila INT,
+	Columna VARCHAR(1),
+	CONSTRAINT FK_SALA_BUTAKA FOREIGN KEY (Id_Sala,ID_Sucursal) REFERENCES SALA(Id_Sala,Id_sucursal),
+	CONSTRAINT PK_BUTACA PRIMARY KEY (id_butaca)
+);
 
 
-	CREATE TABLE Entrada (
-		ID_Entrada INT PRIMARY KEY NOT NULL,
-		ID_Butaca VARCHAR(10) FOREIGN KEY REFERENCES Butaca(ID_Butaca),
-		ID_Funcion INT FOREIGN KEY REFERENCES Funciones(ID_Funcion),
-		DNI INT FOREIGN KEY REFERENCES Usuario(DNI),
-		Precio MONEY,
-	);
-
-
+CREATE TABLE Entrada (
+	ID_Entrada INT NOT NULL ,
+	ID_Butaca VARCHAR(10) FOREIGN KEY REFERENCES Butaca(ID_Butaca),
+	ID_Funcion INT FOREIGN KEY REFERENCES Funciones(ID_Funcion),
+	DNI INT FOREIGN KEY REFERENCES Usuario(DNI),
+	Precio MONEY
+	CONSTRAINT PK_Entrada PRIMARY KEY (id_butaca,id_entrada,id_funcion)
+);
