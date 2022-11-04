8d9dcadc115e352f1da61d86e48e060fb38cfaef
TinoFerrucci <110922321+TinoFerrucci@users.noreply.github.com>
2022-11-04 20:01:09 -0300

    Update Database_Queries.sql

--- Database_Queries.sql
+++ Database_Queries.sql
@@ -1,37 +1,37 @@
 -- Ejercicio A
 select count(id_funcion) as Recuento 
 from Funciones 
-where ID_Sala in (31,32,33)
+where ID_Sucursal = 3
 
 -- Ejercicio B
 Select Nombre 
 from Pelicula as P, Funciones as F 
-where P.ID_Pelicula = F.ID_Pelicula and F.Horario = '15:00:00' and  F.ID_Sala in (21,22,23)
+where P.ID_Pelicula = F.ID_Pelicula and F.Fecha = '2022/10/26' and  F.ID_Sucursal = 2
 
 -- Ejercicio C
 select Horario 
 from Funciones 
-where ID_Sala in (11,12,13) and Fecha = '2022/10/24'
+where ID_Sucursal = 1 and ID_Pelicula = 3101 and Fecha = '2022/10/24'
 
 -- Ejercicio D
-select Fecha, Horario 
-from funciones 
-where ID_Pelicula = 3101
-order by Fecha asc
+select ID_Sucursal as Sucursal, Horario 
+from Funciones 
+where ID_Pelicula = 3101 and Fecha = '2022/10/24'
+order by sucursal, Horario ASC
 
 -- Ejercicio E
 select P.nombre, F.fecha, F.horario 
 from Funciones as F, Pelicula as P 
-where (F.ID_Sala in (11,12,13)) and (F.ID_Pelicula = P.ID_Pelicula) and (F.Fecha between '2022/10/24' and '2022/10/31') and P.ID_Genero = 1
+where F.ID_Sucursal = 1 and F.ID_Pelicula = P.ID_Pelicula and F.Fecha between '2022/10/24' and '2022/10/31' and P.ID_Genero = 1
 
 -- Ejercicio F
-select E.id_Entrada as CantidadDeEntradas 
+select count(E.id_Entrada) as CantidadDeEntradas 
 from Entrada as E
-where (ID_Funcion = 11023) 
+where (ID_Funcion = 25) 
 
 -- Ejercicio G
-Select ID_Butaca from Butaca where ID_Sala = 32
-and ID_Butaca not in (Select ID_Butaca from Entrada)
+Select ID_Butaca as ButacasLibres from Butaca where ID_Sala = 22
+and ID_Butaca not in (Select ID_Butaca from Entrada where ID_Funcion = 8)
 
 -- Ejercicio H
 select P.Nombre, G.Nombre as Genero 
