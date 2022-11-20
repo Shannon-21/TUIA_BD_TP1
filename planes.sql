f6252e4770f84c87dbeb05e262c7b7dc90d5a575
Shannon <81629492+Shannon-21@users.noreply.github.com>
2022-11-20 15:42:16 -0300

    Update planes.sql

--- planes.sql
+++ planes.sql
@@ -1,31 +1,46 @@
 --Creación tabla Planes
 
-CREATE TABLE [dbo].[Planes](
-	[ID_Plan] [int] IDENTITY(1,1) NOT NULL,
-	[Plan] [varchar](15) NOT NULL,
-	[Descripcion] [varchar](100) NOT NULL,
- CONSTRAINT [PK_Planes] PRIMARY KEY (ID_Plan))
- 
---Plan gratis
- select * from pelicula where Premium != 1 and APT = 0
- --Plan premium
- select * from pelicula where APT = 0 
- --Plan familiar
- select * from Pelicula 
- 
- --Entendemos que en el plan gratiuto no se pueden ver películas Premium, pero tuvimos que considerar no mostrar tampoco películas infantiles,
- --ya que de lo contrario no habría diferencia entre el plan premium y familiar.
+CREATE TABLE Planes (
+	ID_Plan int IDENTITY(1,1) NOT NULL,
+	Nombre varchar(15) NOT NULL,
+	Descripcion varchar(100) NOT NULL,
+	CONSTRAINT PK_Planes PRIMARY KEY (ID_Plan)
+	);
 
- select * from usuario
+
+CREATE TABLE Suscripciones (
+	ID_Usuario int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	Fecha_Alta DATE PRIMARY KEY NOT NULL,
+	ID_Plan int IDENTITY(1,1) NOT NULL,
+	Tipo BIT
+);
+ 
 
  --Agregar propiedad "Premium" para saber si la película pertenece al plan premium
-ALTER TABLE Pelicula ADD Premium BIT NOT NULL DEFAULT 0
-UPDATE Pelicula SET Premium = 1 WHERE ID_Pelicula IN (1001, 4390, 9110, 4578, 9458)
+
+ALTER TABLE Peliculas ADD Premium BIT NOT NULL DEFAULT 0
+
+UPDATE Peliculas SET Premium = 1 
+WHERE ID_Pelicula IN (1001, 4390, 9110, 4578, 9458)
+
 --Entendemos que las peliculas con APT = 1 son infantiles
+
 --Agregar contraseña a los usuarios
-ALTER TABLE Usuario ADD Clave VARCHAR(10) NOT NULL DEFAULT '#@!F3HSA"'
+ALTER TABLE Usuarios ADD Clave VARCHAR(10) NOT NULL DEFAULT '#@!F3HSA"'
 
-update Usuario set Clave = 'fabio <3' where dni = 44721345
 
-update Usuario set Clave = '#@!F3HSA"' where clave = '0123456789'
+INSERT INTO Planes (Nombre, Descripcion)
+VALUES ('Gratuito', '(acceso restringido a 10 pel´ıculas por mes)'),
+	   ('Premium', '(acceso a algunas pel´ıculas en cartelera)'),
+	   ('Familiar', '(incluye las pel´ıculas del plan premium, y pel´ıculas infantiles)')
 
+
+CREATE TABLE Suscripciones (
+	DNI int NOT NULL,
+	FechaAlta date NOT NULL,
+	ID_Plan int NOT NULL,
+	Tipo bit NOT NULL,
+	CONSTRAINT PK_Suscripciones PRIMARY KEY( DNI, FechaAlta),
+	CONSTRAINT FK_Suscripciones_Usuarios FOREIGN KEY (DNI) REFERENCES Usuarios,
+	CONSTRAINT FK_Suscripciones_Plan FOREIGN KEY (ID_Plan) REFERENCES Planes
+);
